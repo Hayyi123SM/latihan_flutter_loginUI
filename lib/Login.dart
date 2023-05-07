6eb2513a1b84c749b8f31d6bc2a925d56fc42d33
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2023-05-07 10:40:54 +0700

    feat: membuat komentar

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_ui/dashboard.dart';
 class LoginDemo extends StatefulWidget {
   @override
   _LoginDemoState createState() => _LoginDemoState();
@@ -8,16 +9,22 @@ class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      // Membuat backround dari scaffold menjadi abu-abu dengan kadar 100
       backgroundColor: Colors.grey[100],
+      // Membuat agar content bisa di scroll
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            // Logo Login
             Padding(
+            // Memberi jarak berupa padding untuk bagian atas
               padding: const EdgeInsets.only(top: 60.0),
+              // Mengatur container di tengah
               child: Center(
                 child: Container(
                   width: 200,
                   height: 200,
+                  // Menampilkan gambar dan membuatnya menjadi bundar dengan radius
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(100.0),
                     child: Image.asset('assets/logo_login.png'),
@@ -25,10 +32,14 @@ class _LoginDemoState extends State<LoginDemo> {
                 ),
               ),
             ),
+            // Box kosong untuk mengatur jarak antara widget
             SizedBox(height: 20),
+            // TextField dari Email
             Padding(
+              // Mengatur padding secara horizontal
               // padding: const EdgeInsets.only(left:15.0,right:15.0,top:0,bottom:0),
               padding: EdgeInsets.symmetric(horizontal: 20),
+              // Membuat inputan text Email
               child: TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
@@ -37,10 +48,15 @@ class _LoginDemoState extends State<LoginDemo> {
                 ),
               ),
             ),
+            // Box kosong untuk mengatur jarak antara widget
             SizedBox(height: 13),
+            // TextField dari Password
             Padding(
+              // Mengatur padding secara horizontal
               padding: EdgeInsets.symmetric(horizontal: 20),
+              // Membuat inputan password
               child: TextField(
+                // Membuat agar karakter dari inputan password di hide
                 obscureText: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
@@ -49,9 +65,12 @@ class _LoginDemoState extends State<LoginDemo> {
                 ),
               ),
             ),
+            // Box kosong untuk mengatur jarak antara widget
             SizedBox(height: 13),
             Padding(
+              // Mengatur padding secara horizontal
               padding: EdgeInsets.symmetric(horizontal: 15),
+              // Membuat sebuah text untuk user lupa password
               child: TextButton(
                 onPressed: (){
                   //  TODO FORGOT PASSWORD
@@ -62,26 +81,34 @@ class _LoginDemoState extends State<LoginDemo> {
                 ),
               ),
             ),
+            // Box kosong untuk mengatur jarak antara widget
             SizedBox(height: 10,),
             Padding(
+              // Mengatur padding secara horizontal
               padding: EdgeInsets.symmetric(horizontal: 20),
+              // Membuat tombol Login yang panjang nya mengikuti ukuran layar
               child: SizedBox(
                 height: 50,
                 width: double.infinity,
                 child: ElevatedButton(
                   onPressed: (){
-
+                    // Navigasi ketika tombol login di tekan
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => Dashboard()));
                   },
                   child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 20),),
                 ),
               ),
             ),
+            // Box kosong untuk mengatur jarak antara widget
             SizedBox(
               height: 145,
             ),
             Padding(
+              // Mengatur padding secara horizontal
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: Row(
+                // Mengatur agar widget dari text berada di tengah
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('New user?'),
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -9,23 +9,28 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Appbar atau header dari scaffold dashboard
       appBar: AppBar(
         title: Text("Dashboard"),
+        // mengatur ketinggian atau tebal bayangan
         elevation: .1,
         backgroundColor: Color.fromRGBO(49, 87, 110, 1.0),
       ),
       body: Container(
+        // Membuat padding atau jarak berupa padding untuk garis horizontal dan vertikal
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+        // membuat grid atau sebuah kolom secara vertikal ataupun horizontal
         child: GridView.count(
           crossAxisCount: 2,
           padding: EdgeInsets.all(3.0),
           children: <Widget>[
-            makeDashboardItem("Ordbog", Icons.book),
-            makeDashboardItem("Alphabet", Icons.alarm),
-            makeDashboardItem("Alphabet", Icons.alarm),
-            makeDashboardItem("Alphabet", Icons.alarm),
-            makeDashboardItem("Alphabet", Icons.alarm),
-            makeDashboardItem("Alphabet", Icons.alarm)
+            // memanggil widget card yang sudah di buat
+            makeDashboardItem("Book", Icons.book),
+            makeDashboardItem("Book", Icons.book),
+            makeDashboardItem("Book", Icons.book),
+            makeDashboardItem("Book", Icons.book),
+            makeDashboardItem("Book", Icons.book),
+            makeDashboardItem("Book", Icons.book)
           ],
         ),
       ),
@@ -34,25 +39,33 @@ class _DashboardState extends State<Dashboard> {
 
   Card makeDashboardItem(String title, IconData icon) {
     return Card(
+      // mengatur ketinggian bayangan
         elevation: 1.0,
+        // mengatur jarak antar card dengan margin
         margin: new EdgeInsets.all(8.0),
         child: Container(
+          // memberi dekorasi berupa warna pada masing - masing card
           decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+          // memberi efek splash saat widget di tap
           child: new InkWell(
             onTap: () {},
             child: Column(
+              // fungsi ini akan membuat card mengikuti ukuran layar dan hasilnya akan tetap seperti ini
               crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisSize: MainAxisSize.min,
               verticalDirection: VerticalDirection.down,
               children: <Widget>[
+                // box kosong untuk memberi jarak
                 SizedBox(height: 50.0),
+                // mengatur icon agar berada di tengah - tengah pembungkusnya
                 Center(
                     child: Icon(
                       icon,
                       size: 40.0,
                       color: Colors.black,
                     )),
-                SizedBox(height: 20.0),
+                // box kosong untuk memberi jarak
+                // SizedBox(height: 20.0),
                 new Center(
                   child: new Text(title,
                       style:
